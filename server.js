ab294cab98d55aa85550f6f00ab59add025b39ba
Jaime Barbosa <jaimebs@gmail.com>
2020-03-27 09:54:24 -0300

    refactor: Modificado estrutura para o servidor reconhecer arquivos estáticos.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
 
+// Habilita o express para receber requisições do tipo Json.
 app.use(express.json());
+// Configurar o servidor para apresentar arquivos estáticos.
+app.use(express.static('public'));
+
+// Configurando o template engine.
+nunjucks.configure('./', {
+  express: app
+});
 
 app.get('/', (req, res) => {
-  res.json({ messagem: 'hello word' });
+  res.render('index.html');
 });
 
 app.listen(3333, () => {
