f65c6258148a2298a42954eae39c9e2aa1ee6334
Jaime Barbosa <jaimebs@gmail.com>
2020-03-27 15:03:01 -0300

    refactor: Ajustes e melhorias.

--- server.js
+++ server.js
@@ -22,33 +22,22 @@ nunjucks.configure('./', {
   noCache: true
 });
 
-let donors = [];
-
 app.get('/', (req, res) => {
-  donors = [];
   const sql = `SELECT * FROM Donors`;
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    rows.forEach(donor => {
-      donors.push(donor);
-    });
-    res.render('index.html', { donors });
+  db.all(sql, [], (error, result) => {
+    if (error) return res.send(error.message);
+    const donors = result;
+    return res.render('index.html', { donors });
   });
 });
 
 app.post('/', (req, res) => {
   const { name, email, blood } = req.body;
 
-  const sql = `INSERT INTO Donors (name, email, blood) VALUES
-  ('${name}', '${email}', '${blood}');`;
-  db.run(sql, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    donors.push(req.body);
-    res.redirect('/');
+  const sql = `INSERT INTO Donors (name, email, blood) VALUES ($1, $2, $3);`;
+  db.run(sql, [name, email, blood], error => {
+    if (error) return res.send(error.message);
+    return res.redirect('/');
   });
 });
 
