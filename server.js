b25562992835939d70a6db0d86bede244e61bc27
Jaime Barbosa <jaimebs@gmail.com>
2020-03-27 15:13:56 -0300

    refactor: Ajustes e melhorias.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
 const nunjucks = require('nunjucks');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+
 const path = require('path');
 const dbPath = path.resolve(__dirname, 'db', 'doesangue.db');
 
-let sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(dbPath);
+const db = new sqlite3.Database(dbPath);
 
 // Criando tabela no SQlite assim que sobre o servidor.
 require('./config/created-table')(db);
@@ -22,24 +23,7 @@ nunjucks.configure('./', {
   noCache: true
 });
 
-app.get('/', (req, res) => {
-  const sql = `SELECT * FROM Donors`;
-  db.all(sql, [], (error, result) => {
-    if (error) return res.send(error.message);
-    const donors = result;
-    return res.render('index.html', { donors });
-  });
-});
-
-app.post('/', (req, res) => {
-  const { name, email, blood } = req.body;
-
-  const sql = `INSERT INTO Donors (name, email, blood) VALUES ($1, $2, $3);`;
-  db.run(sql, [name, email, blood], error => {
-    if (error) return res.send(error.message);
-    return res.redirect('/');
-  });
-});
+require('./routes')(app, db);
 
 app.listen(3333, () => {
   console.log('Servidor no ar!');
